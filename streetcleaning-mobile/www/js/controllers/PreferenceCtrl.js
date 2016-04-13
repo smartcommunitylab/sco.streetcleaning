407531e211f663bd9fbfa5ff1a98d42be30c8253
Nawaz Khurshid <nawaz1981@gmail.com>
2016-04-13 18:26:13 +0200

    Added preference + search pages.

--- streetcleaning-mobile/www/js/controllers/PreferenceCtrl.js
+++ streetcleaning-mobile/www/js/controllers/PreferenceCtrl.js
@@ -1,4 +1,30 @@
 angular.module('streetcleaning.controllers.preference', [])
-    .controller('PreferenceCtrl', function($scope, $state, $ionicPopup, $timeout, $filter) {
+    .controller('PreferenceCtrl', function($scope, $state, $ionicPopup, $timeout, $filter, HomeSrv) {
+
+        var successMarkers = function(favoriteMarkers) {
+            $scope.markers = favoriteMarkers;
+        }
+
+        var failureMarkers = function(error) {
+
+        }
+
+        HomeSrv.getFavoriteMarkers().then(successMarkers, failureMarkers);
+
+        $scope.showMarkerDetails = function(arg1, arg2) {
+            $state.go('app.markerDetails', {
+                marker: JSON.stringify(arg1),
+                runningDate: arg2
+            });
+        }
+
+        $scope.removeFavorite = function(marker) {
+            if (marker.favorite) {
+                marker.favorite = false;
+            } else {
+                marker.favorite = true;
+            } 
+        }
+
 
     })
